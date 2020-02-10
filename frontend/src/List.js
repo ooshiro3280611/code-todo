3659ea2d8beacabc21158e9f4478953bc302ba10
ooshiro3280611 <58359684+ooshiro3280611@users.noreply.github.com>
2020-02-10 10:06:04 +0900

    todo-appの作成、編集機能を追加

---
+++ frontend/src/List.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Item from './Item'
+
+const List = ({ todos, deleteTodo, editingTodo }) => {
+    return (
+        <ul>
+            {
+                todos.map((todo, index) => (
+                    <Item
+                        content={todo.content}
+                        id={todo._id}
+                        key={todo._id}
+                        deleteTodo={deleteTodo}
+                        number={index}
+                        editingTodo={editingTodo}
+                    />
+                ))
+            }
+        </ul>
+    )
+}
+
+export default List
